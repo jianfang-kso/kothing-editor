94c861e2d120b1fbe41f15caa4e89908a2235e21
kothing <kothing@outlook.com>
2020-08-23 19:45:12 +0800

    update

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 const merge = require('webpack-merge')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -8,20 +8,23 @@ const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
-	mode: 'production',
-	entry: './src/index',
-	output: {
-		filename: 'kothing-editor.min.js',
-		path: path.resolve(__dirname, '../build')
-	},
+  mode: 'production',
+  entry: './src/index',
+  output: {
+    filename: 'kothing-editor.min.js',
+    path: path.resolve(__dirname, '../build'),
+  },
 
-	plugins: [
-		new webpack.HashedModuleIdsPlugin(),
-		new webpack.optimize.ModuleConcatenationPlugin(),
-		new CleanWebpackPlugin(['../build'], { root: __dirname, allowExternal: true }),
-		new OptimizeCSSPlugin(),
-		new MiniCssExtractPlugin({
-			filename: 'css/kothing-editor.min.css'
-		})
-	]
-});
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CleanWebpackPlugin(['../build'], {
+      root: __dirname,
+      allowExternal: true,
+    }),
+    new OptimizeCSSPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/kothing-editor.min.css',
+    }),
+  ],
+})
