8f826ff9536ea0748669194e10e1eb9e76cee108
Kothing <39289309+kothing@users.noreply.github.com>
2020-09-03 10:01:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import './assets/css/kothing-editor.css'
-import './assets/css/kothing-editor-contents.css'
+import './assets/css/kothing-editor.css';
+import './assets/css/kothing-editor-contents.css';
 
-import plugins from './plugins'
-import KothingEditor from './editor'
+import plugins from './plugins';
+import KothingEditor from './editor';
 
 window.KothingEditor = KothingEditor.init({
   plugins: plugins,
-})
+});
+
+export default KothingEditor;
