d3a635e1ec4f9a3bc5ae345de18f0400f7beab52
Kothing <39289309+kothing@users.noreply.github.com>
2020-09-16 12:03:26 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,6 +58,24 @@ module.exports = {
     "semi-spacing": ["error", {
       "before": false,
       "after": true
-    }]
+    }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        accessibility: "no-public",
+      },
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "off",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    '@typescript-eslint/no-var-requires': "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
